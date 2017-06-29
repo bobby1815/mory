2ac316cc110a8380409c54ab9b71b416093a9d9b
Jindragon <Jindragon@DESKTOP-TC9RNNV>
2017-06-29 16:57:23 +0900

    용진 테스트

--- MORY/src/com/kh/mory/UnitTest.java
+++ MORY/src/com/kh/mory/UnitTest.java
@@ -2,13 +2,6 @@ package com.kh.mory;
 
 import java.sql.SQLException;
 
-/*
-private	int community_type_code, community_love_cnt, community_hits; // 작성번호 , 커뮤니티 유형코드 , 사랑해요 수 , 조회수
-private String write_user_id , page_code 
-, write_cont, write_reg_dtm , community_title, community_type_name;    // 사용자 ID ,페이지구분코드, 작성내용, 작성일시,제목, 커뮤니티유형명
-private int uplo_seq;  //첨부파일 번호
-private String uplo_loca ,write_seq  ;// 파일경로
-*/
 public class UnitTest 
 {
 	public static void main(String[] args) throws SQLException 
