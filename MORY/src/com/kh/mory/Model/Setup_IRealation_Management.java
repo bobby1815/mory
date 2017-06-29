67d4cf672bb03280ea1f76538ad20692b7c811ff
bobby1815 <bobby1815@naver.com>
2017-06-29 13:50:44 +0900

    mybatis jar 파일 추가 setup_realtion_management_DTO 변경

--- MORY/src/com/kh/mory/Model/Setup_IRealation_Management.java
+++ MORY/src/com/kh/mory/Model/Setup_IRealation_Management.java
@@ -1,6 +1,12 @@
 package com.kh.mory.Model;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface Setup_IRealation_Management
 {
-	public int Neighbor (Setup_Realation_ManagementDTO dto);
+	public ArrayList<Setup_Realation_ManagementDTO> searchuser (String user_id) throws SQLException;
+	public int Negihbor(String user_Id, String nei_user_id) throws SQLException;
+	public int UnNegibor(String user_Id, String nei_user_Id) throws SQLException; 
+	
 }
