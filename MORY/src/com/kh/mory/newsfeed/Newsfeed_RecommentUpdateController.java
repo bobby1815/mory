93de6de15aae64e5fa2bea7eb665cda4d3309440
AhhhhhhhhYoung <AhhhhhhhhYoung@DESKTOP-VED562N>
2017-07-02 19:15:28 +0900

    ㅗ

--- MORY/src/com/kh/mory/newsfeed/Newsfeed_RecommentUpdateController.java
+++ MORY/src/com/kh/mory/newsfeed/Newsfeed_RecommentUpdateController.java
@@ -6,8 +6,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-import com.kh.mory.DiaryDTO.DiaryDTO;
-import com.kh.mory.DiaryIDAO.IRecommentDAO;
 
 public class Newsfeed_RecommentUpdateController implements Controller
 {
@@ -39,7 +37,7 @@ public class Newsfeed_RecommentUpdateController implements Controller
 				newsfeedDTO.setRecomment_cont(reco_cont);
 				newsfeedDTO.setRecomment_seq(reco_seq);
 				recommentdao.newsFeedRecommentUpdate(newsfeedDTO);
-				modelAndView.setViewName("redirect:diarypost.do?write_seq="+write_seq);
+				modelAndView.setViewName("redirect:newsfeed_post.do?write_seq="+write_seq);
 
 			} catch (Exception e)
 			{
